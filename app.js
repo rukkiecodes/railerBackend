d4d8be4129390075f35ecd14fbc1da4edf6d0123
rukkiecodes <rukkiecodes@gmail.com>
2021-09-27 15:38:28 +0100

    finished moving google auth to router

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const cors = require("cors")
 const bodyParser = require("body-parser")
 const passport = require("passport")
 const cookieSession = require("cookie-session")
+const morgan = require("morgan")
 require("./passportSetup")
 
 /*
@@ -19,11 +19,22 @@ IMPORT ROUTES
 */
 const isLoggedIn = require("./api/middlewares/isloggedin")
 
-app.use(cors())
+app.use(morgan("dev"))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.use(
   cookieSession({
@@ -38,13 +49,28 @@ app.use(passport.session())
 app.get("/", (req, res) => res.send("Not loggedin"))
 app.get("/failed", (req, res) => res.send("Login failed"))
 
-// @ts-ignore
 app.get("/good", isLoggedIn, (req, res) =>
+  // @ts-ignore
   res.send(`Welcome Mr ${req.user.displayName}`)
 )
 
 app.use("/auth", googleAuth)
 app.use("/auth", googleCallback)
 app.use("/auth", logout)
+
+app.use((req, res, next) => {
+  const error = new Error("Not found")
+  error.status(404)
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message,
+    },
+  })
+})
 
 module.exports = app
