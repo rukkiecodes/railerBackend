52f4df2a5fcd5f13725fc78c630e615c1003687f
rukkiecodes <rukkiecodes@gmail.com>
2021-09-29 14:19:42 +0100

    done with login with email and password

--- api/routes/auth/login.js
+++ api/routes/auth/login.js
@@ -1,8 +1,56 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+const User = require("../../modules/userSchema")
+const jwt = require("jsonwebtoken")
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body
+
+  User.find({ email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        })
+      }
+      bcrypt.compare(password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          })
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          )
+          return res.status(200).json({
+            message: "Auth successful",
+            data: user,
+            token,
+            status: 200
+          })
+        }
+        return res.status(401).json({
+          message: "Auth failed",
+        })
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(400).json({
+        message: "Error finding user",
+        error: err,
+      })
+    })
 })
 
 module.exports = router
