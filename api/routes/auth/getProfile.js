3639e81978f89e058e852f1286e0a09d313e4b9c
rukkiecodes <rukkiecodes@gmail.com>
2021-10-02 03:24:31 +0100

    can now get profile

--- api/routes/auth/getProfile.js
+++ api/routes/auth/getProfile.js
@@ -1,10 +1,22 @@
 const express = require("express")
+const { error } = require("npmlog")
 const router = express.Router()
+const User = require("../../modules/userSchema")
 
 router.get("/profile", (req, res) => {
-  res.status(200).json({
-    message: "Get profile",
-  })
+  const { registrationEmail } = req.body
+  User.find({ registrationEmail })
+    .exec()
+    .then((data) => {
+      return res.status(200).json({
+        data,
+      })
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+      })
+    })
 })
 
 module.exports = router
