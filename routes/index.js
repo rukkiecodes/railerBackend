6e493426c681b133eb8e9b64ddd30260f147a8de
rukkiecodes <rukkiecodes@gmail.com>
2021-10-04 03:27:38 +0100

    added vue and vue tify to projects

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require("../middlewares/auth")
 
 // @desc Login/Landing page
 // @route GET /
 
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   })
@@ -13,8 +14,10 @@ router.get("/", (req, res) => {
 // @desc Login/Dashboard
 // @route GET / dashboard
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard")
+router.get("/dashboard", ensureAuth, (req, res) => {
+  res.render("dashboard", {
+    name: req.user.firstName
+  })
 })
 
 module.exports = router
