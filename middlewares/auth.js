a10ca3f0353dd95885dea711830fc8dd11b3e45c
rukkiecodes <rukkiecodes@gmail.com>
2021-10-07 22:46:18 +0100

    refactor auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,16 @@
 module.exports = {
-  ensureAuth: function (req, res, next) {
+  ensureAuth: (req, res, next) => {
     if (req.isAuthenticated()) {
       return next()
     } else {
       res.redirect('/')
     }
   },
-  ensureGuest: function (req, res, next) {
+  ensureGuest: (req, res, next) => {
     if (!req.isAuthenticated()) {
       return next();
     } else {
-      res.redirect('/dashboard');
+      res.redirect("/template")
     }
   },
 }
