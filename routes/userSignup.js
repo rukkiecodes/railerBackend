efc52b4108ab11c4c94c7df4ecbcba76e27e526e
rukkiecodes <rukkiecodes@gmail.com>
2021-10-07 01:49:12 +0100

    checked if user is already social authenticated

--- routes/userSignup.js
+++ routes/userSignup.js
@@ -3,26 +3,39 @@ const router = express.Router()
 const bcrypt = require("bcrypt")
 const mongoose = require("mongoose")
 const Signup = require("../models/Signup")
+const User = require("../models/User")
 
 router.post("/", async (req, res) => {
   req.body._id = new mongoose.Types.ObjectId()
   try {
-    let user = await Signup.findOne({ email: req.body.email })
+    const user = await User.findOne({ email: req.body.email })
     if (user) {
-      console.log("User exist")
+      console.log("Google user exist")
+      res.redirect("/signup")
     } else {
-      await bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          console.log(err)
+      console.log("Google user does not exist")
+      try {
+        let signup = await Signup.findOne({ email: req.body.email })
+        if (signup) {
+          console.log("User exist")
+          res.redirect("/signup")
         } else {
-          user = Signup.create({
-            _id: req.body._id,
-            name: req.body.name,
-            email: req.body.email,
-            password: hash,
+          await bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              console.log(err)
+            } else {
+              signup = Signup.create({
+                _id: req.body._id,
+                name: req.body.name,
+                email: req.body.email,
+                password: hash,
+              })
+            }
           })
         }
-      })
+      } catch (error) {
+        console.error(error)
+      }
     }
   } catch (error) {
     console.error(error)
