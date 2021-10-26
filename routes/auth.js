21319986a493cc8f12f5dffc4f34b5e95535abd6
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-26 01:06:01 +0100

    signup works

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,43 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+const mongoose = require("mongoose")
 
-router.post("/signup", (req, res) => {
-  res.status(200).json({
-    message: "Signup"
-  })
+const User = require("../models/User")
+
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body
+  try {
+    let user = await User.findOne({ email })
+
+    if (user) {
+      console.log("User exists")
+      res.status(409).json({
+        message: "Auth failed",
+      })
+    } else {
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          res.status(409).json({
+            message: "Auth failed",
+          })
+        } else {
+          let newUser = {
+            _id: new mongoose.Types.ObjectId(),
+            email,
+            password: hash,
+          }
+          user = User.create(newUser)
+
+          res.status(201).json({
+            message: "Auth successful",
+          })
+        }
+      })
+    }
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 module.exports = router
